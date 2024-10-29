9ae23d015969e979851727060a1964e5391e2fe8
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-10-29 14:54:47 +0530

    Add project files.

---
+++ Maxarray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxarray
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            int[] x = new int[5];
+
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine("Enter number");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            int max = x[0];
+            for (int i = 0; i <x.Length; i++)
+            {
+              
+                if (x[i] > max)
+                {
+                    max = x[i];
+                }
+                
+            }
+            Console.WriteLine("Maximum number in given array is=" + max);
+            Console.ReadLine();
+        }
+    }
+}
